38c943ad9715bef6dc4763c93352d9ed17334721
Shawn Meyer <shmeyer@microsoft.com>
2026-01-13 17:19:02 -0500

    image build update

--- deployments/Invoke-ImageBuilds.ps1
+++ deployments/Invoke-ImageBuilds.ps1
@@ -1,17 +1,25 @@
-$Date = Get-Date -Format 'yyyyMMddhhmmss'
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$Location,
+    [Parameter(Mandatory = $false)]
+    [array]$ParameterFilePrefixes = @()
+)
 
-$Prefixes = @('CustomPrefix1')
 $DeploymentJobs = @()
-ForEach ($Prefix in $Prefixes) {
+ForEach ($Prefix in $ParameterFilePrefixes) {
     $ParameterFile = Join-Path -Path $PSScriptRoot -ChildPath "imageManagement\imageBuild\parameters\$Prefix.imagebuild.parameters.json"
     If (Test-Path -Path $ParameterFile) {
         Write-Output "Using parameter file: $ParameterFile"
-        $DeploymentJobs += New-AzDeployment -Name "ImageBuild-$Prefix-$Date" -Location 'eastus2' -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\imageBuild.json') -TemplateParameterFile $ParameterFile -AsJob 
+        $Date = Get-Date -Format 'yyyyMMddhhmmss'
+        $DeploymentJob = New-AzDeployment -Name "ImageBuild-$Prefix-$Date" -Location 'eastus2' -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\imageBuild.json') -TemplateParameterFile $ParameterFile -AsJob 
+        Start-Sleep -Seconds 1
     }
     else {
         Write-Error "Parameter file $ParameterFile does not exist. Please create the parameter file and try again."
         exit
     }
+    $DeploymentJobs += $DeploymentJob
 }
 
 Wait-Job -Job $DeploymentJobs
